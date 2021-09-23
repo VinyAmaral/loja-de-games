db2806d54f38ccf1d241a1f84b6544b37009d597
Vinicius <vg5642@gmail.com>
2021-09-22 23:43:36 -0300

    com o security

---
+++ src/main/java/br/org/generation/minhalojadegames/model/Categoria.java
@@ -0,0 +1,69 @@
+package br.org.generation.minhalojadegames.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull(message = "O atributo tipo é obrigatório")
+	private String tipo;
+	
+	@NotNull(message = "O atributo recomendação é obrigatório")
+	private String recomendacao;
+	
+	@OneToMany(mappedBy ="categoria" , cascade = CascadeType.ALL)
+	@JsonIgnoreProperties("categoria")
+	private List <Produto> produto;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getRecomendacao() {
+		return recomendacao;
+	}
+
+	public void setRecomendacao(String recomendacao) {
+		this.recomendacao = recomendacao;
+	}
+
+	public List<Produto> getProduto() {
+		return produto;
+	}
+
+	public void setProduto(List<Produto> produto) {
+		this.produto = produto;
+	}
+	
+	
+
+}
+
